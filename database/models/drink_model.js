7c7d5d25ebdc42d4c3be3371c1a39c26c1f84b50
David Ramsdale <davidramsdale1991@gmail.com>
2018-12-18 09:45:32 +1100

    Added Drink model

--- database/models/drink_model.js
+++ database/models/drink_model.js
@@ -1,1 +1,6 @@
 const mongoose = require("mongoose");
+const DrinkSchema = require("./../schema/drink_schema");
+
+const DrinkModel = mongoose.model("drink", DrinkSchema);
+
+module.exports = DrinkModel;
